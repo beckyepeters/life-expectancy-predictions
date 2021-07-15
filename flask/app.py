6d8c4e1bad8dfea710d8543b4f6564046a59e87b
Becky Peters <beckyepeters@gmail.com>
2021-07-14 22:01:18 -0600

    wed night max depth tuning

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
-%matplotlib inline
 import seaborn as sns   
 from pivottablejs import pivot_ui
 import datetime
@@ -22,9 +21,35 @@ import joblib
 
 app = Flask(__name__) 
 
-loaded_model = load('')
+loaded_model = joblib.load('model_rf_age_adjusted_wonder.sav')
 
 @app.route('/')
-
 def index(): 
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict(): 
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='You can expect to die at the age of {}'.format(output))
+
+
+def model(): 
+    inputs = request.args
+    results = loaded_model.predict([inputs['input_1']])
+    answer = int(results)
+    return answer
+
+@app.route('/results',methods=['POST'])
+def results(): 
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__": 
+    app.run(debug=True)
