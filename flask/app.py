3aee70f29ce888af23272841b9b188a4a7d346e6
Becky Peters <beckyepeters@gmail.com>
2021-07-16 00:14:49 -0600

    flask done, presentation in progress

--- flask/app.py
+++ flask/app.py
@@ -1,60 +1,36 @@
 from flask import Flask, render_template, request, jsonify
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-import seaborn as sns   
-from pivottablejs import pivot_ui
-import datetime
-from sklearn.impute import KNNImputer
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.decomposition import PCA
-
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
-
 import joblib
 import json
 import requests
 
 app = Flask(__name__) 
+loaded_model = joblib.load('final_model.pkl')
 
-@app.route('/')
+
+@app.route('/') 
 def index(): 
-    return render_template('index.html')
+    return render_template('index.html')    
 
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,7)
-    loaded_model = joblib.load('final_model.sav')
-    result = loaded_model.predict(to_predict)
-    return result[0]
 
-# @app.route('/model', methods=['POST'])
-# def model():
-#     inputs = request.get_json()
-#     prediction = np.array2string(loaded_model.predict([inputs])
-#     return jsonify(prediction)
+@app.route('/', methods=["POST"])
+def model(): 
+    inputs = []
+    for idx in ['race', 'sex', 'ms', 'hisp', 'adjinc', 'educ', 'hitype']:
+        input = request.form.get(idx)
+        inputs.append(input)
+    results = loaded_model.predict([inputs])
+    results = str(results)
+    return f'Based on 2002 mortality data, you can expect to die at age {results}'
 
-@app.route('/result',methods=['POST'])
-def result():
-    if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
-        result = ValuePredictor(to_predict_list) 
-    return render_template("result", prediction=result)
-
-    output = prediction[0]
-    return jsonify(output)
+# @app.route('/result')
+# def result(): 
+#     return render_template('result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-url = 'http://localhost:5000/results'
-r = requests.post(url)
-
-print(r.json())
